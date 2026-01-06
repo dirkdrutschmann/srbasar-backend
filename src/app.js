b52f36cb7f1fc88b685f314320e5d76dd4e89cf8
Dirk <55602317+dirkdrutschmann@users.noreply.github.com>
2026-01-06 11:50:24 +0100

    Add 'sentry-trace' and 'baggage' to allowedHeaders
    Error-Handling, via Sentry

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'sentry-trace', 'baggage'],
   preflightContinue: false,
   optionsSuccessStatus: 200
 }));
